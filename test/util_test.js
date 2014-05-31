802e73d68388aba7a142c280b30b967156b60507
Brian Donovan <donovan@squareup.com>
2014-05-31 13:17:09 -0400

    Prevent infinite loops when iterating over non-arrayish objects.

--- test/util_test.js
+++ test/util_test.js
@@ -496,15 +496,15 @@ describe('#callGetIterator', function() {
       '    var index = 0;' +
       '    return {' +
       '        next: function() {' +
-      '            if (index >= array.length) {' +
-      '                return {' +
-      '                    done: true,' +
-      '                    value: void 0' +
-      '                };' +
-      '            } else {' +
+      '            if (index < array.length) {' +
       '                return {' +
       '                    done: false,' +
       '                    value: array[index++]' +
+      '                };' +
+      '            } else {' +
+      '                return {' +
+      '                    done: true,' +
+      '                    value: void 0' +
       '                };' +
       '            }' +
       '        }' +
